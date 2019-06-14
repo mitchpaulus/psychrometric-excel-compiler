9f63d1e552f059da202b5d98d7322ca2397cbcd2
Mitchell Paulus <paulusm14@gmail.com>
2019-06-14 16:22:32 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,8 @@
+# Psychrometric Excel Compiler
+
+This web page will be used to generate the psychrometric formulas
+dynamically for Excel. This way, add-ins and VBA can completely be
+avoided.
+
+This project is not finished quite yet.
+
---
+++ psy.ts
@@ -0,0 +1,17 @@
+const c8: number = -1.0440397e4;
+const c9: number = -1.129465e1;
+const c10: number = -2.7022355e-2;
+const c11: number = 1.289036e-5;
+const c12: number = -2.4780681e-9;
+const c13: number = 6.5459673;
+
+class viewModel {
+    
+
+
+
+    constructor() {
+        
+    }
+}
+
